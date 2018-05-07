4cb79ef4a3259ba2a70b7212715afce1a976f47c
hemajun815 <411388997@qq.com>
2018-05-07 11:39:51 +0800

    Update '数组中出现次数超过一半的数字'

--- 029.cc
+++ 029.cc
@@ -8,28 +8,44 @@ class Solution {
 public:
     int MoreThanHalfNum_Solution(vector<int> numbers) {
         map<int, int> m;
+        int num = 0, count = 0;
         for (int i = 0; i < numbers.size(); ++i)
         {
             int number = numbers.at(i);
             map<int, int>::iterator iter = m.find(number);
             if (iter->first == number)
+            {
                 iter->second += 2;
+                if (iter->second > count)
+                {
+                    num = number;
+                    count = iter->second;
+                }
+            }
             else
+            {
                 m.insert(pair<int, int>(number, 2));
+                if (2 > count)
+                {
+                    num = number;
+                    count = 2;
+                }
+            }
         }
-        for (map<int, int>::iterator iter=m.begin(); iter!=m.end(); iter++)
-        {
-            if (iter->second > numbers.size())
-                return iter->first;
-        }
+        if (count > numbers.size())
+            return num;
         return 0;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    int a[] = {1};
-    vector<int> vct(a, a+5);
+    vector<int> vct;
+    vct.push_back(1);
+    vct.push_back(2);
+    vct.push_back(1);
+    vct.push_back(3);
+    vct.push_back(1);
     cout << (new Solution())->MoreThanHalfNum_Solution(vct) << endl;
     return 0;
 }
